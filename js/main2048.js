32ba0c02fe88a324a7fc4e7fa011de22a27441d7
tinywall <1051261239@qq.com>
2017-10-03 14:49:43 +0800

    新增手机端游玩功能

--- js/main2048.js
+++ js/main2048.js
@@ -1,17 +1,31 @@
 var board = new Array();
 var score = 0;
 var hasConflicted = new Array(); //用来判断每个格子是否已经发生过碰撞，从而避免一下子好几个格子相加;
+
+//新增
+var startx = 0; //移动端触摸屏幕时开始点的x坐标
+var starty = 0; //移动端触摸屏幕时开始点的y坐标
+var endx = 0; //移动端触摸屏幕时结束点的x坐标
+var endy = 0; //移动端触摸屏幕时结束点的y坐标
+
+
+
 $(document).ready(function() {
-    newGame();
+
+  //新增,做自适应处理
+  prepareForMobile();
+
+  newGame();
 });
 
 function newGame() {
-    $('.score').empty();
-    //初始化棋盘格
-    init();
-    //随机在两个格子生成数字
-    generateOneNumber();
-    generateOneNumber();
+  $('.score').empty();
+
+  //初始化棋盘格
+  init();
+  //随机在两个格子生成数字
+  generateOneNumber();
+  generateOneNumber();
 }
 
 /*
@@ -20,264 +34,367 @@ function newGame() {
  *  3、初始化数据 清零 updateBoardView();
  */
 function init() {
-    for (var i = 0; i < 4; i++) {
-        board[i] = new Array();
-        hasConflicted[i] = new Array();
-        for (var j = 0; j < 4; j++) {
-            var gridCell = $('#grid_cell_' + i + '_' + j);
-            // var x={
-            // 	'top': getPosition(i),
-            // 	'left': getPosition(j)
-            // }
-            // gridCell.css(x);
-            gridCell.css({ 'top': getPosition(i), 'left': getPosition(j) });
-            //console.log('#grid_cell_' + i + '_' + j);
-            //console.log(getPosition(i), getPosition(j));
-            board[i][j] = 0;
-            hasConflicted[i][j] = false;
-        }
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      var grid_cell = $('#grid_cell_' + i + '_' + j);
+      grid_cell.css('top', getPosition(i));
+      grid_cell.css('left', getPosition(j));
+      //console.log('#grid_cell_' + i + '_' + j);
+      //console.log(getPosition(i), getPosition(j));
     }
-    updateBoardView();
-    score = 0;
-    updateScore(score);
+  }
+  for (var i = 0; i < 4; i++) {
+    board[i] = new Array();
+    hasConflicted[i] = new Array();
+    for (var j = 0; j < 4; j++) {
+      board[i][j] = 0;
+      hasConflicted[i][j] = false;
+    }
+  }
+  updateBoardView();
+  score = 0;
+  updateScore(score);
 }
 
 // 初始化数据,将数据可视化,根据board[i][j]存的数值来画图
 function updateBoardView() {
-    $('.number_cell').remove();
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
-            $('#grid_container').append('<div class="number_cell" id="number_cell_' + i + '_' + j + '"></div>');
-            var theNumberCell = $('#number_cell_' + i + '_' + j);
-            if (board[i][j] == 0) {
-                var numberCellCSS = {
-                    "width": '0px',
-                    "height": '0px',
-                    "top": getPosition(i) + 50,
-                    "left": getPosition(j) + 50
-                }
-                //console.log(getPosition(i), getPosition(j));
-                theNumberCell.css(numberCellCSS);
-            } else {
-                var numberCellCSS = {
-                    "width": "100px",
-                    "height": "100px",
-                    "top": getPosition(i),
-                    "left": getPosition(j),
-                    "background-color": getNumberBackgroundColor(board[i][j]),
-                    "color": getNumberColor(board[i][j]),
-                    'font-size': getNumberFontSize(board[i][j])
-                }
-                theNumberCell.css(numberCellCSS);
-                theNumberCell.text(board[i][j]);
-            }
-            hasConflicted[i][j] = false;
+  $('.number_cell').remove();
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      $('#grid_container').append('<div class="number_cell" id="number_cell_' + i + '_' + j + '"></div>');
+      var theNumberCell = $('#number_cell_' + i + '_' + j);
+      if (board[i][j] == 0) {
+        var numberCellCSS = {
+          'width': '0px',
+          'height': '0px',
+          'top': getPosition(i) + cellSideLength / 2,
+          'left': getPosition(j) + cellSideLength / 2
         }
+        theNumberCell.css(numberCellCSS);
+        //console.log(getPosition(i), getPosition(j));
+      } else {
+        var numberCellCSS = {
+          'width': cellSideLength,
+          'height': cellSideLength,
+          'top': getPosition(i),
+          'left': getPosition(j),
+          'background-color': getNumberBackgroundColor(board[i][j]),
+          'color': getNumberColor(board[i][j]),
+          'font-size': getNumberFontSize(board[i][j])
+        }
+        theNumberCell.css(numberCellCSS);
+        theNumberCell.text(board[i][j]);
+      }
+      hasConflicted[i][j] = false;
     }
-    //console.log("updateBoardView running...");
+  }
+  $('.number_cell').css('line-height', cellSideLength + 'px');
+  //console.log("updateBoardView running...");
 }
+
 
 // 随机选一个格子生成一个数字
 function generateOneNumber() {
-    if (noSpace(board))
-        return false;
-    //随机位置
-    var randx = parseInt(Math.floor(Math.random() * 4));
-    var randy = parseInt(Math.floor(Math.random() * 4));
+  if (noSpace(board)) {
+    return false;
+  }
+  //随机位置
+  var randx = parseInt(Math.floor(Math.random() * 4));
+  var randy = parseInt(Math.floor(Math.random() * 4));
 
-    // 设置一个时间参数，50次以内系统还未生成一个空位置，那么就进行人工找一个空位置
-    var times = 0;
-    while (times < 50) {
-        if (board[randx][randy] == 0)
-            break;
-
-        randx = parseInt(Math.floor(Math.random() * 4));
-        randy = parseInt(Math.floor(Math.random() * 4));
-
-        times++;
+  // 设置一个时间参数，50次以内系统还未生成一个空位置，那么就进行人工找一个空位置
+  var times = 0;
+  while (times < 50) {
+    if (board[randx][randy] == 0) {
+      break;
     }
-    if (times == 50) {
-        for (var i = 0; i < 4; i++)
-            for (var j = 0; j < 4; j++) {
-                if (board[i][j] == 0) {
-                    randx = i;
-                    randy = j;
-                }
-            }
+    randx = parseInt(Math.floor(Math.random() * 4));
+    randy = parseInt(Math.floor(Math.random() * 4));
+    times++;
+  }
+  if (times == 50) {
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 4; j++) {
+        if (board[i][j] == 0) {
+          randx = i;
+          randy = j;
+        }
+      }
     }
+  }
 
-    //console.log(randx, randy);
-    //随机一个数字
-    var randNumber = Math.random() < 0.5 ? 2 : 4;
-    //console.log(randNumber);
-    board[randx][randy] = randNumber;
-    //在上面找到的随机位置显示该随机数字
-    showNumberWithAnimation(randx, randy, randNumber);
-    //console.log("generateOneNumber running...");
+  //console.log(randx, randy);
+  //随机一个数字
+  var randNumber = Math.random() < 0.5 ? 2 : 4;
+  //console.log(randNumber);
+  board[randx][randy] = randNumber;
+  //在上面找到的随机位置显示该随机数字
+  showNumberWithAnimation(randx, randy, randNumber);
+  return true;
+  //console.log("generateOneNumber running...");
 }
 
-// 判断键盘的响应时间 上下左右
+//新增 自适应处理
+function prepareForMobile() {
+  if (documentWidth > 500) {
+    gridContainerWidth = 500;
+    cellSideLength = 100;
+    cellSpace = 20;
+  }
+  $('#grid_container').css('width', gridContainerWidth - 2 * cellSpace);
+  $('#grid_container').css('height', gridContainerWidth - 2 * cellSpace);
+  $('#grid_container').css('padding', cellSpace);
+  $('#grid_container').css('border-radius', 0.02 * gridContainerWidth);
+  $('.grid_cell').css('width', cellSideLength);
+  $('.grid_cell').css('height', cellSideLength);
+  $('.grid_cell').css('border-radius', 0.02 * gridContainerWidth);
+}
+
+// 监听键盘的上下左右移动
 $(document).keydown(function(event) {
-    event.preventDefault();
-    switch (event.keyCode) {
-        case 37: // left 向左移动
-            if (moveLeft()) {
-                setTimeout("generateOneNumber()", 210);
-                setTimeout("isGameOver()", 300);
-            };
-            break;
-        case 38: // up 向上移动
-            if (moveUp()) {
-                setTimeout("generateOneNumber()", 210);
-                setTimeout("isGameOver()", 300);
-            };
-            break;
-        case 39: // right 向右移动
-            if (moveRight()) {
-                setTimeout("generateOneNumber()", 210);
-                setTimeout("isGameOver()", 300);
-            };
-            break;
-        case 40: // down 向下移动
-            if (moveDown()) {
-                setTimeout("generateOneNumber()", 210);
-                setTimeout("isGameOver()", 300);
-            };
-            break;
-        default: // default
-            break;
-    }
+  event.preventDefault();
+  switch (event.keyCode) {
+    case 37: // left 向左移动
+      if (moveLeft()) {
+        setTimeout("generateOneNumber()", 210);
+        setTimeout("isGameOver()", 300);
+      };
+      break;
+    case 38: // up 向上移动
+      if (moveUp()) {
+        setTimeout("generateOneNumber()", 210);
+        setTimeout("isGameOver()", 300);
+      };
+      break;
+    case 39: // right 向右移动
+      if (moveRight()) {
+        setTimeout("generateOneNumber()", 210);
+        setTimeout("isGameOver()", 300);
+      };
+      break;
+    case 40: // down 向下移动
+      if (moveDown()) {
+        setTimeout("generateOneNumber()", 210);
+        setTimeout("isGameOver()", 300);
+      };
+      break;
+    default: // default
+      break;
+  }
 });
 
-function moveLeft() {
-    //1.先判断能否向左移动
-    if (!canMoveLeft(board))
-        return false;
 
-    /*2.如果可以向左移动：
-     *   ①当前的数字是否为0，不为0则进行左移 board[i][j] != 0
-     *   ②如果左侧为空格子，则数字进行一个移位操作 board[i][k] == 0
-     *   ③如果左侧有数字且不相等，则数字还是进行移位操作 noBlockHorizontal
-     *   ④如果左侧有数字且相等，则数字进行相加操作
-     */
-    for (var i = 0; i < 4; i++) {
-        for (var j = 1; j < 4; j++) {
-            if (board[i][j] != 0) {
-                for (var k = 0; k < j; k++) {
-                    if (board[i][k] == 0 && noBlockHorizontal(i, k, j, board)) {
-                        showMoveAnimation(i, j, i, k);
-                        board[i][k] = board[i][j];
-                        board[i][j] = 0;
-                        continue;
-                    } else if (board[i][k] == board[i][j] && noBlockHorizontal(i, k, j, board) && !hasConflicted[i][k]) {
-                        showMoveAnimation(i, j, i, k); //移动
-                        board[i][k] += board[i][j]; //相加
-                        board[i][j] = 0;
-                        score += board[i][k];
-                        updateScore(score); //更新分数
-                        hasConflicted[i][k] = true;
-                        continue;
-                    }
-                }
-            }
-        }
+
+//新赠
+//touchstart：当手指触摸屏幕时候触发，即使已经有一个手指放在屏幕上也会触发。
+//touchmove：当手指在屏幕上滑动的时候连续地触发。在这个事件发生期间，调用preventDefault()事件可以阻止滚动。
+//touchend：当手指从屏幕上拿起的时候触发。
+//touchcancel：当系统停止（取消）跟踪touch触摸的时候触发。
+//
+//
+//每个触摸事件都包括了三个触摸列表（即用于跟踪触摸的属性），每个列表里包含了对应的一系列触摸点（用来实现多点触控）：
+//touches：当前位于屏幕上的所有手指的一个列表（表示当前跟踪的触摸操作的touch对象的数组）
+//targetTouches：位于当前DOM元素上的手指的一个列表（特定于事件目标的Touch对象的数组）
+//changeTouches：涉及当前事件的手指的一个列表（表示自上次触摸以来发生了什么改变的Touch对象的数组）
+//
+//
+//监听移动设备的触摸开始
+document.addEventListener('touchstart', function(event) {
+  startx = event.touches[0].pageX;
+  starty = event.touches[0].pageY;
+});
+
+//监听移动设备的触摸移动
+document.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+});
+
+//监听移动设备的触摸结束
+document.addEventListener('touchend', function(event) {
+  endx = event.changedTouches[0].pageX;
+  endy = event.changedTouches[0].pageY;
+
+  var deltax = endx - startx;
+  var deltay = endy - starty;
+  if (Math.abs(deltax) < 0.3 * documentWidth && Math.abs(deltay) < 0.3 * documentWidth) {
+    return;
+  }
+
+  if (Math.abs(deltax) >= Math.abs(deltay)) {
+    if (deltax > 0) {
+      //move right
+      if (moveRight()) {
+        setTimeout('generateOneNumber()', 210);
+        setTimeout('isGameOver()', 300);
+      }
+    } else {
+      //move left
+      if (moveLeft()) {
+        setTimeout('generateOneNumber()', 210);
+        setTimeout('isGameOver()', 300);
+      }
     }
-    // 3、初始化数据 updateBoardView()
-    // 为显示动画效果，设置该函数的等待时间200ms
-    setTimeout("updateBoardView()", 200);
-    return true;
+  } else { //y
+    if (deltay > 0) {
+      //move down
+      if (moveDown()) {
+        setTimeout('generateOneNumber()', 210);
+        setTimeout('isGameOver()', 300);
+      }
+    } else {
+      //move up
+      if (moveUp()) {
+        setTimeout('generateOneNumber()', 210);
+        setTimeout('isGameOver()', 300);
+      }
+    }
+  }
+});
+
+
+
+function moveLeft() {
+  //1.先判断能否向左移动
+  if (!canMoveLeft(board)) {
+    return false;
+  }
+
+  /*2.如果可以向左移动：
+   *   ①当前的数字是否为0，不为0则进行左移 board[i][j] != 0
+   *   ②如果左侧为空格子，则数字进行一个移位操作 board[i][k] == 0
+   *   ③如果左侧有数字且不相等，则数字还是进行移位操作 noBlockHorizontal
+   *   ④如果左侧有数字且相等，则数字进行相加操作
+   */
+  for (var i = 0; i < 4; i++) {
+    for (var j = 1; j < 4; j++) {
+      if (board[i][j] != 0) {
+        for (var k = 0; k < j; k++) {
+          if (board[i][k] == 0 && noBlockHorizontal(i, k, j, board)) {
+            showMoveAnimation(i, j, i, k);
+            board[i][k] = board[i][j];
+            board[i][j] = 0;
+            break;
+          } else if (board[i][k] == board[i][j] && noBlockHorizontal(i, k, j, board) && !hasConflicted[i][k]) {
+            showMoveAnimation(i, j, i, k); //移动
+            board[i][k] += board[i][j]; //相加
+            board[i][j] = 0;
+            score += board[i][k];
+            updateScore(score); //更新分数
+            hasConflicted[i][k] = true;
+            break;
+          }
+        }
+      }
+    }
+  }
+  // 3、初始化数据 updateBoardView()
+  // 为显示动画效果，设置该函数的等待时间200ms
+  setTimeout("updateBoardView()", 200);
+  return true;
 }
 
+
 function moveRight() {
-    if (!canMoveRight(board))
-        return false;
-    for (var i = 0; i < 4; i++) {
-        for (var j = 2; j >= 0; j--) {
-            if (board[i][j] != 0) {
-                for (var k = 3; k > j; k--) {
-                    if (board[i][k] == 0 && noBlockHorizontal(i, j, k, board)) {
-                        showMoveAnimation(i, j, i, k);
-                        board[i][k] = board[i][j];
-                        board[i][j] = 0;
-                        continue;
-                    } else if (board[i][k] == board[i][j] && noBlockHorizontal(i, j, k, board) && !hasConflicted[i][k]) {
-                        showMoveAnimation(i, j, i, k); //移动
-                        board[i][k] += board[i][j]; //相加
-                        board[i][j] = 0;
-                        score += board[i][k];
-                        updateScore(score); //更新分数
-                        hasConflicted[i][k] = true;
-                        continue;
-                    }
-                }
-            }
+  if (!canMoveRight(board)) {
+    return false;
+  }
+  for (var i = 0; i < 4; i++) {
+    for (var j = 2; j >= 0; j--) {
+      if (board[i][j] != 0) {
+        for (var k = 3; k > j; k--) {
+          if (board[i][k] == 0 && noBlockHorizontal(i, j, k, board)) {
+            showMoveAnimation(i, j, i, k);
+            board[i][k] = board[i][j];
+            board[i][j] = 0;
+            break;
+          } else if (board[i][k] == board[i][j] && noBlockHorizontal(i, j, k, board) && !hasConflicted[i][k]) {
+            showMoveAnimation(i, j, i, k); //移动
+            board[i][k] += board[i][j]; //相加
+            board[i][j] = 0;
+            score += board[i][k];
+            updateScore(score); //更新分数
+            hasConflicted[i][k] = true;
+            break;
+          }
         }
+      }
     }
-    setTimeout("updateBoardView()", 200);
-    return true;
+  }
+  setTimeout("updateBoardView()", 200);
+  return true;
 }
 
 function moveUp() {
-    if (!canMoveUp(board))
-        return false;
-    for (var j = 0; j < 4; j++) {
-        for (var i = 1; i < 4; i++) {
-            if (board[i][j] != 0) {
-                for (var k = 0; k < i; k++) {
-                    if (board[k][j] == 0 && noBlockVertical(j, k, i, board)) {
-                        showMoveAnimation(i, j, k, j);
-                        board[k][j] = board[i][j];
-                        board[i][j] = 0;
-                        continue;
-                    } else if (board[k][j] == board[i][j] && noBlockHorizontal(j, k, i, board) && !hasConflicted[k][j]) {
-                        showMoveAnimation(i, j, k, j); //移动
-                        board[k][j] += board[i][j]; //相加
-                        board[i][j] = 0;
-                        score += board[k][j];
-                        updateScore(score); //更新分数
-                        hasConflicted[k][j] = true;
-                        continue;
-                    }
-                }
-            }
+  if (!canMoveUp(board)) {
+    return false;
+  }
+  for (var j = 0; j < 4; j++) {
+    for (var i = 1; i < 4; i++) {
+      if (board[i][j] != 0) {
+        for (var k = 0; k < i; k++) {
+          if (board[k][j] == 0 && noBlockVertical(j, k, i, board)) {
+            showMoveAnimation(i, j, k, j);
+            board[k][j] = board[i][j];
+            board[i][j] = 0;
+            break;
+          } else if (board[k][j] == board[i][j] && noBlockHorizontal(j, k, i, board) && !hasConflicted[k][j]) {
+            showMoveAnimation(i, j, k, j); //移动
+            board[k][j] += board[i][j]; //相加
+            board[i][j] = 0;
+            score += board[k][j];
+            updateScore(score); //更新分数
+            hasConflicted[k][j] = true;
+            break;
+          }
         }
+      }
     }
-    setTimeout("updateBoardView()", 200);
-    return true;
+  }
+  setTimeout("updateBoardView()", 200);
+  return true;
 }
 
 function moveDown() {
-    if (!canMoveDown(board))
-        return false;
-    for (var j = 0; j < 4; j++) {
-        for (var i = 2; i >= 0; i--) {
-            if (board[i][j] != 0) {
-                for (var k = 3; k > i; k--) {
-                    if (board[k][j] == 0 && noBlockVertical(j, i, k, board)) {
-                        showMoveAnimation(i, j, k, j);
-                        board[k][j] = board[i][j];
-                        board[i][j] = 0;
-                        continue;
-                    } else if (board[k][j] == board[i][j] && noBlockHorizontal(j, i, k, board) && !hasConflicted[k][j]) {
-                        showMoveAnimation(i, j, k, j); //移动
-                        board[k][j] += board[i][j]; //相加
-                        board[i][j] = 0;
-                        score += board[k][j];
-                        updateScore(score); //更新分数
-                        hasConflicted[k][j] = true;
-                        continue;
-                    }
-                }
-            }
+  if (!canMoveDown(board)) {
+    return false;
+  }
+  for (var j = 0; j < 4; j++) {
+    for (var i = 2; i >= 0; i--) {
+      if (board[i][j] != 0) {
+        for (var k = 3; k > i; k--) {
+          if (board[k][j] == 0 && noBlockVertical(j, i, k, board)) {
+            showMoveAnimation(i, j, k, j);
+            board[k][j] = board[i][j];
+            board[i][j] = 0;
+            break;
+          } else if (board[k][j] == board[i][j] && noBlockHorizontal(j, i, k, board) && !hasConflicted[k][j]) {
+            showMoveAnimation(i, j, k, j); //移动
+            board[k][j] += board[i][j]; //相加
+            board[i][j] = 0;
+            score += board[k][j];
+            updateScore(score); //更新分数
+            hasConflicted[k][j] = true;
+            break;
+          }
         }
+      }
     }
-    setTimeout("updateBoardView()", 200);
-    return true;
+  }
+  setTimeout("updateBoardView()", 200);
+  return true;
 }
 
 //判断游戏是否结束
 function isGameOver() {
-    if (noSpace(board) && noMove(board)) {
-        gameOver();
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      if (board[i][j] == 2048) {
+        gameSucess();
+        return;
+      }
     }
+  }
+  if (noSpace(board) && noMove(board)) {
+    gameOver();
+  }
 }
--- js/support2048.js
+++ js/support2048.js
@@ -1,79 +1,74 @@
+//新增
+documentWidth = window.screen.availWidth; //屏幕宽度
+gridContainerWidth = 0.92 * documentWidth; //期盘宽度
+cellSideLength = 0.18 * documentWidth; //格子的大小
+cellSpace = 0.04 * documentWidth; //格子之间的间隔
+
+
 //获取每个单元格的坐标
 function getPosition(index) {
-    return 20 + index * 120 ;
+  return cellSpace + index * (cellSpace + cellSideLength);
 }
 
 // 设置不同数字的不同背景颜色
 function getNumberBackgroundColor(number) {
-    switch (number) {
-        case 2:
-            return "#eee4da";
-            break;
-        case 4:
-            return "#ede0c8";
-            break;
-        case 8:
-            return "#f2b179";
-            break;
-        case 16:
-            return "#f59563";
-            break;
-        case 32:
-            return "#f67c5f";
-            break;
-        case 64:
-            return "#f65e3b";
-            break;
-        case 128:
-            return "#edcf72";
-            break;
-        case 256:
-            return "#edcc61";
-            break;
-        case 512:
-            return "#9c0";
-            break;
-        case 1024:
-            return "#33b5e5";
-            break;
-        case 2048:
-            return "#09c";
-            break;
-        case 4096:
-            return "#a6c";
-            break;
-        case 8192:
-            return '#93c';
-            break;
-    }
-    return "black";
+  switch (number) {
+    case 2:
+      return "#eee4da";
+    case 4:
+      return "#ede0c8";
+    case 8:
+      return "#f2b179";
+    case 16:
+      return "#f59563";
+    case 32:
+      return "#f67c5f";
+    case 64:
+      return "#f65e3b";
+    case 128:
+      return "#edcf72";
+    case 256:
+      return "#edcc61";
+    case 512:
+      return "#9c0";
+    case 1024:
+      return "#33b5e5";
+    case 2048:
+      return "#09c";
+    case 4096:
+      return "#a6c";
+    case 8192:
+      return '#93c';
+  }
+  return "black";
 }
 
 // 设置数字的颜色：2和4的颜色都为#776e65，其它数字的颜色为白色
 function getNumberColor(number) {
-    if (number <= 4)
-        return "#776e65";
-
-    return "white";
+  if (number <= 4) {
+    return "#776e65";
+  }
+  return "white";
 }
 
 //设置数字字体大小:2和4为60px,其他为40px
 function getNumberFontSize(number) {
-    if (number <= 512)
-        return '60px';
-    return "40px";
+  if (number <= 512) {
+    return 0.6 * cellSideLength;
+  }
+  return 0.4 * cellSideLength;
 }
 
 // 判断棋盘是否还有空格子
 function noSpace(board) {
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
-            if (board[i][j] == 0) {
-                return false;
-            }
-        }
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      if (board[i][j] == 0) {
+        return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
 
 /* 判断能否向左移动
@@ -84,82 +79,91 @@ function noSpace(board) {
  *   或者左侧格子和当前格子数字相同，即 board[i][j - 1] == board[i][j]
  */
 function canMoveLeft(board) {
-    for (var i = 0; i < 4; i++) {
-        for (var j = 1; j < 4; j++) {
-            if (board[i][j] != 0) {
-                if (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j]) {
-                    return true;
-                }
-            }
+  for (var i = 0; i < 4; i++) {
+    for (var j = 1; j < 4; j++) {
+      if (board[i][j] != 0) {
+        if (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j]) {
+          return true;
         }
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 //判断是否能向右、向上、向下移动
 function canMoveRight(board) {
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 3; j++) {
-            if (board[i][j] != 0) {
-                if (board[i][j + 1] == 0 || board[i][j + 1] == board[i][j]) {
-                    return true;
-                }
-            }
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 3; j++) {
+      if (board[i][j] != 0) {
+        if (board[i][j + 1] == 0 || board[i][j + 1] == board[i][j]) {
+          return true;
         }
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function canMoveUp(board) {
-    for (var j = 0; j < 4; j++) {
-        for (var i = 1; i < 4; i++) {
-            if (board[i][j] != 0) {
-                if (board[i - 1][j] == 0 || board[i - 1][j] == board[i][j]) {
-                    return true;
-                }
-            }
+  for (var j = 0; j < 4; j++) {
+    for (var i = 1; i < 4; i++) {
+      if (board[i][j] != 0) {
+        if (board[i - 1][j] == 0 || board[i - 1][j] == board[i][j]) {
+          return true;
         }
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 function canMoveDown(board) {
-    for (var j = 0; j < 4; j++) {
-        for (var i = 0; i < 3; i++) {
-            if (board[i][j] != 0) {
-                if (board[i + 1][j] == 0 || board[i + 1][j] == board[i][j]) {
-                    return true;
-                }
-            }
+  for (var j = 0; j < 4; j++) {
+    for (var i = 0; i < 3; i++) {
+      if (board[i][j] != 0) {
+        if (board[i + 1][j] == 0 || board[i + 1][j] == board[i][j]) {
+          return true;
         }
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 // 判断水平方向的两个目标格子之间有没有其他数字
 function noBlockHorizontal(row, col1, col2, board) {
-    for (var i = col1 + 1; i < col2; i++)
-        if (board[row][i] != 0)
-            return false; // 如果在这两者之间的其它格子有数字，返回false
-    return true; // 如果两者之间没数字，返回true
+  for (var i = col1 + 1; i < col2; i++)
+    if (board[row][i] != 0) {
+      return false; // 如果在这两者之间的其它格子有数字，返回false
+    }
+  return true; // 如果两者之间没数字，返回true
 }
 
 // 判断垂直方向的两个目标格子之间没有其他数字
 function noBlockVertical(col, row1, row2, board) {
-    for (var i = row1 + 1; i < row2; i++)
-        if (board[i][col] != 0)
-            return false; // 如果在这两者之间的其它格子有数字，返回false
-    return true; // 如果两者之间没数字，返回true
+  for (var i = row1 + 1; i < row2; i++)
+    if (board[i][col] != 0) {
+      return false; // 如果在这两者之间的其它格子有数字，返回false
+    }
+  return true; // 如果两者之间没数字，返回true
 }
 
 //判断4个方向是否还能移动
 function noMove(board) {
-    if (canMoveLeft(board) || canMoveRight(board) || canMoveUp(board) || canMoveDown(board))
-        return false;
-    return true;
+  if (canMoveLeft(board) || canMoveRight(board) || canMoveUp(board) || canMoveDown(board)) {
+    return false;
+  }
+  return true;
+}
+
+//新增
+//游戏成功
+function gameSucess() {
+  $('.score').html('<p style="color: green;">Sucess</p>');
 }
 
 //游戏结束
-function gameOver(){
-    $('.score').html('<p style="color: red;">Game Over</p>');
+function gameOver() {
+  $('.score').html('<p style="color: red;">Game Over</p>');
 }
