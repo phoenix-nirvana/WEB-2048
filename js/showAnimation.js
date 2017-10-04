798dc2e0ccdc3008fbc1b519ef774f27f1529f68
tinywall <1051261239@qq.com>
2017-10-05 01:33:38 +0800

    修复前一次修改导致的严重错误

--- js/showAnimation.js
+++ js/showAnimation.js
@@ -3,39 +3,23 @@
  * 在这个显示过程中 设置了一个50毫秒的动画效果
  */
 function showNumberWithAnimation(i, j, number) {
-<<<<<<< HEAD
-  var numberCell = $('#number_cell_' + i + '_' + j);
-  var numberCellCSS = {
-    'background-color': getNumberBackgroundColor(number),
-    'color': getNumberColor(number),
-    'font-size': getNumberFontSize(number)
-  };
-  numberCell.css(numberCellCSS);
-  numberCell.text(number);
-  numberCell.animate({
-    width: cellSideLength,
-    height: cellSideLength,
-    top: getPosition(i),
-    left: getPosition(j)
-  }, 50);
-  //console.log("showNumberWithAnimation running...");
-=======
     var numberCell = $('#number_cell_' + i + '_' + j);
     var numberCellCSS = {
         'background-color': getNumberBackgroundColor(number),
         'color': getNumberColor(number),
-        'font-size': getNumberFontSize(number)
-    }
+ //       'font-size': getNumberFontSize(number)
+    };
     numberCell.css(numberCellCSS);
     numberCell.text(number);
     numberCell.animate({
-        width: cellSideLength,
-        height: cellSideLength,
+        // width: cellSideLength,
+        // height: cellSideLength,
+        width: '100px',
+        height: '100px',
         top: getPosition(i),
         left: getPosition(j)
     }, 50);
     //console.log("showNumberWithAnimation running...");
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 }
 
 /* 移动数字的动画：
@@ -43,14 +27,14 @@ function showNumberWithAnimation(i, j, number) {
  * 在这个显示过程中 设置了一个200毫秒的动画效果
  */
 function showMoveAnimation(fromx, fromy, tox, toy) {
-  var numberCell = $('#number_cell_' + fromx + '_' + fromy);
-  numberCell.animate({
-    top: getPosition(tox),
-    left: getPosition(toy)
-  }, 200);
+    var numberCell = $('#number_cell_' + fromx + '_' + fromy);
+    numberCell.animate({
+        top: getPosition(tox),
+        left: getPosition(toy)
+    }, 200);
 }
 
 // 分数刷新显示，此处未设置动画
 function updateScore(score) {
-  $('#score').text(score);
+    $('#score').text(score);
 }
--- js/support2048.js
+++ js/support2048.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 //新增
-=======
-//新赠
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 documentWidth = window.screen.availWidth; //屏幕宽度
 gridContainerWidth = 0.92 * documentWidth; //期盘宽度
 cellSideLength = 0.18 * documentWidth; //格子的大小
@@ -11,86 +7,68 @@ cellSpace = 0.04 * documentWidth; //格子之间的间隔
 
 //获取每个单元格的坐标
 function getPosition(index) {
-<<<<<<< HEAD
-  return cellSpace + index * (cellSpace + cellSideLength);
-=======
     return cellSpace + index * (cellSpace + cellSideLength);
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 }
 
 // 设置不同数字的不同背景颜色
 function getNumberBackgroundColor(number) {
-  switch (number) {
-    case 2:
-      return "#eee4da";
-    case 4:
-      return "#ede0c8";
-    case 8:
-      return "#f2b179";
-    case 16:
-      return "#f59563";
-    case 32:
-      return "#f67c5f";
-    case 64:
-      return "#f65e3b";
-    case 128:
-      return "#edcf72";
-    case 256:
-      return "#edcc61";
-    case 512:
-      return "#9c0";
-    case 1024:
-      return "#33b5e5";
-    case 2048:
-      return "#09c";
-    case 4096:
-      return "#a6c";
-    case 8192:
-      return '#93c';
-  }
-  return "black";
+    switch (number) {
+        case 2:
+            return "#eee4da";
+        case 4:
+            return "#ede0c8";
+        case 8:
+            return "#f2b179";
+        case 16:
+            return "#f59563";
+        case 32:
+            return "#f67c5f";
+        case 64:
+            return "#f65e3b";
+        case 128:
+            return "#edcf72";
+        case 256:
+            return "#edcc61";
+        case 512:
+            return "#9c0";
+        case 1024:
+            return "#33b5e5";
+        case 2048:
+            return "#09c";
+        case 4096:
+            return "#a6c";
+        case 8192:
+            return '#93c';
+    }
+    return "black";
 }
 
 // 设置数字的颜色：2和4的颜色都为#776e65，其它数字的颜色为白色
 function getNumberColor(number) {
-<<<<<<< HEAD
-  if (number <= 4) {
-    return "#776e65";
-  }
-  return "white";
-=======
-    if (number <= 4){
+    if (number <= 4) {
         return "#776e65";
     }
     return "white";
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 }
 
 //设置数字字体大小:2和4为60px,其他为40px
 function getNumberFontSize(number) {
-<<<<<<< HEAD
-  if (number <= 512) {
-    return 0.6 * cellSideLength;
-  }
-  return 0.4 * cellSideLength;
-=======
-    if (number <= 512){
+    if (number <= 512) {
         return 0.6 * cellSideLength;
     }
     return 0.4 * cellSideLength;
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 }
 
 // 判断棋盘是否还有空格子
 function noSpace(board) {
-  for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 4; j++) {
-      if (board[i][j] == 0) {
-        return false;
-      }
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            if (board[i][j] == 0) {
+                return false;
+            }
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 /* 判断能否向左移动
@@ -101,125 +79,96 @@ function noSpace(board) {
  *   或者左侧格子和当前格子数字相同，即 board[i][j - 1] == board[i][j]
  */
 function canMoveLeft(board) {
-  for (var i = 0; i < 4; i++) {
-    for (var j = 1; j < 4; j++) {
-      if (board[i][j] != 0) {
-        if (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j]) {
-          return true;
+    for (var i = 0; i < 4; i++) {
+        for (var j = 1; j < 4; j++) {
+            if (board[i][j] != 0) {
+                if (board[i][j - 1] == 0 || board[i][j - 1] == board[i][j]) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
 }
 
 //判断是否能向右、向上、向下移动
 function canMoveRight(board) {
-  for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 3; j++) {
-      if (board[i][j] != 0) {
-        if (board[i][j + 1] == 0 || board[i][j + 1] == board[i][j]) {
-          return true;
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (board[i][j] != 0) {
+                if (board[i][j + 1] == 0 || board[i][j + 1] == board[i][j]) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
 }
 
 function canMoveUp(board) {
-  for (var j = 0; j < 4; j++) {
-    for (var i = 1; i < 4; i++) {
-      if (board[i][j] != 0) {
-        if (board[i - 1][j] == 0 || board[i - 1][j] == board[i][j]) {
-          return true;
+    for (var j = 0; j < 4; j++) {
+        for (var i = 1; i < 4; i++) {
+            if (board[i][j] != 0) {
+                if (board[i - 1][j] == 0 || board[i - 1][j] == board[i][j]) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
 }
 
 function canMoveDown(board) {
-  for (var j = 0; j < 4; j++) {
-    for (var i = 0; i < 3; i++) {
-      if (board[i][j] != 0) {
-        if (board[i + 1][j] == 0 || board[i + 1][j] == board[i][j]) {
-          return true;
+    for (var j = 0; j < 4; j++) {
+        for (var i = 0; i < 3; i++) {
+            if (board[i][j] != 0) {
+                if (board[i + 1][j] == 0 || board[i + 1][j] == board[i][j]) {
+                    return true;
+                }
+            }
         }
-      }
     }
-  }
-  return false;
+    return false;
 }
 
 // 判断水平方向的两个目标格子之间有没有其他数字
 function noBlockHorizontal(row, col1, col2, board) {
-<<<<<<< HEAD
-  for (var i = col1 + 1; i < col2; i++)
-    if (board[row][i] != 0) {
-      return false; // 如果在这两者之间的其它格子有数字，返回false
-    }
-  return true; // 如果两者之间没数字，返回true
-=======
     for (var i = col1 + 1; i < col2; i++)
-        if (board[row][i] != 0){
+        if (board[row][i] != 0) {
             return false; // 如果在这两者之间的其它格子有数字，返回false
         }
     return true; // 如果两者之间没数字，返回true
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 }
 
 // 判断垂直方向的两个目标格子之间没有其他数字
 function noBlockVertical(col, row1, row2, board) {
-<<<<<<< HEAD
-  for (var i = row1 + 1; i < row2; i++)
-    if (board[i][col] != 0) {
-      return false; // 如果在这两者之间的其它格子有数字，返回false
-    }
-  return true; // 如果两者之间没数字，返回true
-=======
     for (var i = row1 + 1; i < row2; i++)
-        if (board[i][col] != 0){
+        if (board[i][col] != 0) {
             return false; // 如果在这两者之间的其它格子有数字，返回false
         }
     return true; // 如果两者之间没数字，返回true
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
 }
 
 //判断4个方向是否还能移动
 function noMove(board) {
-<<<<<<< HEAD
-  if (canMoveLeft(board) || canMoveRight(board) || canMoveUp(board) || canMoveDown(board)) {
-    return false;
-  }
-  return true;
+    if (canMoveLeft(board) || canMoveRight(board) || canMoveUp(board) || canMoveDown(board)) {
+        return false;
+    }
+    return true;
 }
 
 //新增
 //游戏成功
 function gameSucess() {
-  $('.score').html('<p style="color: green;">Sucess</p>');
-=======
-    if (canMoveLeft(board) || canMoveRight(board) || canMoveUp(board) || canMoveDown(board)){
+    $('.score').html('<p style="color: green;">Sucess</p>');
+    if (canMoveLeft(board) || canMoveRight(board) || canMoveUp(board) || canMoveDown(board)) {
         return false;
     }
     return true;
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
-}
-
-//新增
-//游戏成功
-function gameSucess(){
-    $('.score').html('<p style="color: green;">Sucess</p>');
 }
 
 //游戏结束
 function gameOver() {
-<<<<<<< HEAD
-  $('.score').html('<p style="color: red;">Game Over</p>');
-}
-=======
     $('.score').html('<p style="color: red;">Game Over</p>');
 }
->>>>>>> e5034614d5abbe0bb75fca91790f01ba2d70018a
+
